1cb4ed9cfe0590b50d411471cd510d522a62c72e
moaaz97 <moaaz.briek97@gmail.com>
2023-03-07 23:31:56 +0200

    Do qr builder method

--- app/Http/Controllers/QrController.php
+++ app/Http/Controllers/QrController.php
@@ -3,11 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class QrController extends Controller
 {
     public function index()
     {
         return view('qr_codes.qr_builder');
+    }
+
+    public function do_qr_builder(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+           'name' => 'required',
+           'body' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        $name = $request->input('name');
+        $body = $request->input('body');
+        $qr_code = QrCode::generate($body);
+
+        return back()->with([
+           'status' => 'Qr Code Generated Successfully',
+           'code' => $qr_code,
+        ]);
     }
 }
