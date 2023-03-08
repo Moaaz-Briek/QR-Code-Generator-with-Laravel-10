f49fcc32deed64ea9e18818a1060b4add19eb2bb
moaaz97 <moaaz.briek97@gmail.com>
2023-03-08 08:38:20 +0200

    Save Qr code as image in public files

--- app/Http/Controllers/QrController.php
+++ app/Http/Controllers/QrController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class QrController extends Controller
@@ -16,8 +17,8 @@ class QrController extends Controller
     public function do_qr_builder(Request $request)
     {
         $validator = Validator::make($request->all(), [
-           'name' => 'required',
-           'body' => 'required',
+            'name' => 'required',
+            'body' => 'required',
         ]);
 
         if ($validator->fails()) {
@@ -25,12 +26,14 @@ class QrController extends Controller
         }
 
         $name = $request->input('name');
+        $image_name = Str::slug($name) . '.png'; //Moaaz Briek => moaaz-briek.png
         $body = $request->input('body');
-        $qr_code = QrCode::generate($body);
+
+        QrCode::format('png')->generate($body, '../public/qr_code/'. $image_name);
 
         return back()->with([
-           'status' => 'Qr Code Generated Successfully',
-           'code' => $qr_code,
+            'status' => 'Qr Code Generated Successfully',
+            'image_name' => $image_name,
         ]);
     }
 }
