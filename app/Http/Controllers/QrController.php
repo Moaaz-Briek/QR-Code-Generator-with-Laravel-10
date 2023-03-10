434f86564d9eb8a7851a6738a6bcf6c2cc8e8603
moaaz97 <moaaz.briek97@gmail.com>
2023-03-10 23:42:22 +0200

    Set the Qr controller to accept different types, and define scan function

--- app/Http/Controllers/QrController.php
+++ app/Http/Controllers/QrController.php
@@ -10,24 +10,91 @@ use Spatie\Color\Hex;
 
 class QrController extends Controller
 {
+    protected string $body;
+    protected string $phone;
+    protected string $qr_type;
+    protected string $image_name;
+    protected string $subject;
+    protected string $to;
+    protected string $phoneNumber;
+    protected string $message;
+    protected string $latitude;
+    protected string $longitude;
+    protected string $encryption;
+    protected string $ssid;
+    protected string $password;
+    protected string $hidden;
+
     public function index()
     {
-        return view('qr_codes.qr_builder');
+        return view('qr_codes.index');
     }
 
     public function do_qr_builder(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'body' => 'required',
-        ]);
-
+        $this->qr_type = $request->input('qr_type');
+        switch ($this->qr_type) {
+            case 'text':
+                $validator = Validator::make($request->all(), [
+                    'body' => 'required',
+                ]);
+                if (!$validator->fails()) {
+                    $this->body = $request->input('body');
+                }
+                break;
+            case 'phone':
+                $validator = Validator::make($request->all(), [
+                    'phone' => 'required',
+                ]);
+                if (!$validator->fails()) {
+                    $this->phone = $request->input('phone');
+                }
+                break;
+            case 'email':
+                $validator = Validator::make($request->all(), [
+                    'to' => 'required|email',
+                ]);
+                if (!$validator->fails()) {
+                    $this->to = $request->input('to');
+                    $this->subject = $request->input('subject') ?? '';
+                    $this->body = $request->input('email_body') ?? '';
+                }
+                break;
+            case 'sms':
+                $validator = Validator::make($request->all(), [
+                    'phoneNumber' => 'required',
+                ]);
+                if (!$validator->fails()) {
+                    $this->phoneNumber = $request->input('phoneNumber');
+                    $this->message = $request->input('message') ?? '';
+                }
+                break;
+            case 'geo':
+                $validator = Validator::make($request->all(), [
+                    'latitude' => 'required',
+                    'longitude' => 'required',
+                ]);
+                if (!$validator->fails()) {
+                    $this->latitude = $request->input('latitude');
+                    $this->longitude = $request->input('longitude');
+                }
+                break;
+            case 'wifi':
+                $validator = Validator::make($request->all(), [
+                    'ssid' => 'required',
+                ]);
+                if (!$validator->fails()) {
+                    $this->encryption = $request->input('encryption') ?? '';
+                    $this->ssid = $request->input('ssid');
+                    $this->password = $request->input('password') ?? '';
+                    $this->hidden = $request->input('hidden') ?? false;
+                }
+                break;
+        }
         if ($validator->fails()) {
             return back()->withErrors($validator)->withInput();
         }
 
-        $name = $request->input('name');
-        $body = $request->input('body');
         $qr_size = $request->input('qr_size') ?? '300';
         $qr_image_type = $request->input('qr_image_type') ?? 'png';
         $qr_correction = $request->input('qr_correction') ?? 'H';
@@ -90,14 +157,31 @@ class QrController extends Controller
             $qr_gradient_end->blue(),
             $qr_gradient_type,
         );
+        $this->image_name = Str::slug(time()) . '.' . $qr_image_type; //Moaaz Briek => moaaz-briek.png
 
-        $image_name = Str::slug($name) . '.' . $qr_image_type; //Moaaz Briek => moaaz-briek.png
-        $qr->generate($body, '../public/qr_code/'. $image_name);
-
-
+        if ($this->qr_type == 'text') {
+            $qr->generate($this->body, '../public/qr_code/'. $this->image_name);
+            $array = ['image_name' => $this->image_name];
+        } elseif ($this->qr_type == 'phone') {
+            $array = ['img' => QrCode::phoneNumber($this->phone)];
+        } elseif ($this->qr_type == 'email') {
+            $array = ['img' => QrCode::email($this->to, $this->subject, $this->body)];
+        } elseif ($this->qr_type == 'sms') {
+            $array = ['img' => QrCode::SMS($this->phoneNumber, $this->message)];
+        } elseif ($this->qr_type == 'geo') {
+            $array = ['img' => QrCode::geo($this->latitude, $this->longitude)];
+        } elseif ($this->qr_type == 'wifi') {
+            $array = ['img' => QrCode::wifi($this->encryption, $this->ssid, $this->password, $this->hidden)];
+        }
         return back()->with([
+            ...$array,
             'status' => 'Qr Code Generated Successfully',
-            'image_name' => $image_name,
+            'type' => $this->qr_type,
         ]);
+    }
+
+    public function scan()
+    {
+        return view('qr_codes.scan');
     }
 }
